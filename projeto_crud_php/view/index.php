de86475ea0f46fa1018065c841809134b0e56b24
João Gustavo <105756115+jpG98@users.noreply.github.com>
2024-04-12 11:07:51 -0300

    Add files via upload

---
+++ projeto_crud_php/view/index.php
@@ -0,0 +1,76 @@
+<?php include("blades/top.php"); ?>
+
+<div class="container mt-5">
+<a href="v_cadastro.php" class="btn btn-primary">cadastrar</a>
+</div>   
+
+<div class="container mt-2 bg-white rounded p-3">
+<table class="table table-bordered table-hover table-striped">
+    <thead>
+        <tr>
+            <th>Código</th>
+            <th>Nome</th>
+            <th>Cidade</th>
+            <th>Curso</th>
+            <th colspan="2">Ação</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>01</td>
+            <td>João Gustavo</td>
+            <td>Registro</td>
+            <td>TDS</td>
+            <td><a href="#" class="btn btn-success">Editar</a></td>
+            <td><a href="#" class="btn btn-danger">Excluir</a></td>
+        </tr>
+    </tbody>
+    <tbody>
+        <tr>
+            <td>01</td>
+            <td>João Gustavo</td>
+            <td>Registro</td>
+            <td>TDS</td>
+            <td><a href="#" class="btn btn-success">Editar</a></td>
+            <td><a href="#" class="btn btn-danger">Excluir</a></td>
+        </tr>
+    </tbody>
+    <tbody>
+        <tr>
+            <td>01</td>
+            <td>João Gustavo</td>
+            <td>Registro</td>
+            <td>TDS</td>
+            <td><a href="#" class="btn btn-success">Editar</a></td>
+            <td><a href="#" class="btn btn-danger">Excluir</a></td>
+        </tr>
+    </tbody>
+    <tbody>
+        <tr>
+            <td>01</td>
+            <td>João Gustavo</td>
+            <td>Registro</td>
+            <td>TDS</td>
+            <td><a href="#" class="btn btn-success">Editar</a></td>
+            <td><a href="#" class="btn btn-danger">Excluir</a></td>
+        </tr>
+    </tbody>
+    <tbody>
+        <tr>
+            <td>01</td>
+            <td>João Gustavo</td>
+            <td>Registro</td>
+            <td>TDS</td>
+            <td><a href="#" class="btn btn-success">Editar</a></td>
+            <td><a href="#" class="btn btn-danger">Excluir</a></td>
+        </tr>
+    </tbody>
+    <Tfoot>
+        <tr>
+            <td colspan="6">Rodapé da Tabela</td>
+        </tr>
+    </Tfoot>
+</table>
+</div>
+
+<?php include("blades/footer.php"); ?>
